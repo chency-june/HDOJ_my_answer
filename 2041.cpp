36aeb4e0677ed58102fca2d57618953babab5e98
chency <chency0579@163.com>
2021-03-06 16:34:10 +0800

    fix2041

--- 2041.cpp
+++ 2041.cpp
@@ -3,22 +3,16 @@
 using namespace std;
 
 int main(void) {
-    int n;
-    cin >> n;
-    while (n--) {
-        int m;
-        cin >> m;
-        m--;
-        long long int times = 0;
-        for (int i = 0; 2 * i <= m; i++) {
-            // C (m-i) i
-            long long int tmp = 1;
-            for (int j = 1; j <= i; j++) {
-                tmp = tmp * ( m - i - j + 1 ) / j;
-            }
-            times += tmp;
-        }
-        cout << times << endl;
+    long long int arr[41] = { 0, 1, 1, 2};
+    for ( int i = 3; i < 41; i++ ) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << arr[n] << endl;
     }
     return 0;
 }
